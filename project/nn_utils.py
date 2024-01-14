46da38f0c6bf0eba8458c7afb03d9df4235b6969
Nsivaa <alberto.dicembre@gmail.com>
2024-01-14 19:26:00 +0100

    almost finished

--- project/nn_utils.py
+++ project/nn_utils.py
@@ -18,33 +18,26 @@ def tanh(x):
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+np.random.seed(0)
 
 class Layer:
     '''
         The Layer class represents a layer of the neural network.
         It has a ndarray of weights and an ndarray of biases, and a boolean "is_input" attribute 
     '''
-    def __init__(self, weights: np.ndarray = None, biases: np.ndarray = None, is_input = False):    
-        if not is_input: #if the layer is an input layer we skip the dimension constraint check
-            self.is_input = False
+    def __init__(self, n_inputs: int = 0, n_neurons: int = 0, is_input : bool = False):    
+        self.is_input = is_input
 
-            if weights is not None and biases is not None and weights.shape[0] != biases.shape[0]:
-                print("ERROR: biases and weights have different dimensions")
-                return
+        if n_neurons == 0:
+            self.weights = np.empty((0,0))
+
         else:
-            self.is_input = True
-        
-        self.weights = weights
-        if self.weights is None:
-            self.weights = np.empty([0,0])
+            self.weights = 0,1 * np.random.randn(n_inputs, n_neurons)
 
-        self.biases = biases
-        if self.biases is None and not self.is_input: 
-            self.biases = np.empty([0])
-
+        self.biases = np.zeros((1, n_neurons))
         return
     
-    ''' TODO: add weight matrix dimension check
+    ''' 
     def add_neuron(self, neuron_weights: np.ndarray = None, neuron_bias: int = None):
         if neuron_weights is not None:
             if len(neuron_weights.shape) > 1:
@@ -55,24 +48,19 @@ class Layer:
         np.append(self.biases, neuron_bias)
     '''
 
+    def feed_forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
     def __len__(self): #returns the number of nodes
      
-        if self.biases is not None:
-            return len(self.biases)
-        elif self.weights is not None:
-            return self.weights.shape[0]
-        return 0
+        return len(self.biases)
 
     def __str__(self):
 
         out_str = ""
-        if self.is_input:
-            for pos, weights in enumerate(el for el in self.weights if el is not None):
-                out_str += f"NODE {pos} VALUES = {str(weights)} \n"  
-        
-        else: 
-            for pos, weights in enumerate(el for el in self.weights if el is not None):
-                out_str += f"NODE {pos} WEIGHTS = str(weights), BIAS = {self.biases[pos]}\n"
+
+        for pos, weights in enumerate(el for el in self.weights[1] if el is not None):
+            out_str += f"NODE {pos} WEIGHTS = {str(weights[pos])}, BIAS = {self.biases[0][pos]}\n"
 
         return out_str
 
@@ -83,84 +71,52 @@ class NeuralNetwork:
         hidden_layers: list of Layer objects
         output_layer: Layer object
     '''
-    def __init__(self, input_layer: Layer = None, hidden_layers: list = None, output_layer : Layer = None):
+    def __init__(self, input_layer: Layer = None, hidden_layers: list = [], output_layer : Layer = None):
         
         self.input_layer = input_layer
         self.hidden_layers = hidden_layers
         self.output_layer = output_layer
 
-        if self.input_layer is None:
-            self.input_layer = Layer() #Empty layer
-        
-        if hidden_layers is None:
-            self.hidden_layers = []
-        
-        if output_layer is None:
-            self.output_layer = Layer()
-
         return
 
-    def add_input_layer(self, n_neurons : int = None):
-        if self.input_layer is not None and self.input_layer.weights.size > 0:
-            print("Warning: input layer already present. Overwriting")
-        
-        weights = np.random.rand(n_neurons)
-        input_layer = Layer(weights=weights, biases=None, is_input=True)
-        self.input_layer = input_layer
+    def add_input_layer(self, n_neurons : int = 0):
+        self.input_layer = Layer(n_inputs = 1, n_neurons = n_neurons, is_input = True)
         return
+    
     ''' 
         Add a new layer in the network. If the position is not specified, it is appended.
         The layer is created with the number of weights for each neuron relative to the previous layer
     '''
-    def add_hidden_layer(self, n_neurons: int = None, pos: int = None):
-        if pos is None:
-            if self.hidden_layers is not None:
-                pos = len(self.hidden_layers)
-
-        if len(self.hidden_layers) == 0 or self.hidden_layers is None:
-            weights = np.random.rand(n_neurons, len(self.input_layer))
+    def add_hidden_layer(self, n_inputs: int = 0, n_neurons: int = 0, pos: int = -1):
+        layer = Layer(n_inputs, n_neurons, is_input = False)
+        if pos == -1:
+            self.hidden_layers.append(layer)
 
         else:
-            weights = np.random.rand(n_neurons, len(self.hidden_layers[pos - 1]))
-
-        biases = np.random.rand(n_neurons)
-        layer = Layer(weights, biases)
-        self.hidden_layers.insert(pos, layer)
+            self.hidden_layers.insert(pos, layer)
         return
         
-    def add_output_layer(self, n_neurons : int = None):
-        if self.output_layer is not None and len(self.output_layer) > 0:
-            print("Warning: output layer already present. Overwriting")
-
-        if self.hidden_layers is None or len(self.hidden_layers) == 0:
-            print("ERROR: no hidden layers, aborting")
-            return
-        
-        weights = np.random.rand(n_neurons, len(self.hidden_layers[-1]))
-        biases = np.random.rand(n_neurons)
-        output_layer = Layer(weights=weights, biases=biases, is_input=False)
-        self.output_layer = output_layer
-        return
+    def add_output_layer(self, n_inputs : int = 0, n_neurons : int = 0):
+        self.output_layer = Layer(n_inputs, n_neurons, is_input = False)
+        return 
     
-
     def neur_feed_forward(self, A_prev : np.ndarray, W : np.ndarray, b : np.ndarray):
             A = np.dot(W, A_prev) + b
             return A
 
     def train(self, data: pd.DataFrame):
-        for row in data.itertuples(index = False, name = None): #iterrows instead??
+        for row in data.itertuples(index = False, name = None): #iterrows instead?? 
             self.input_layer.weights = np.asarray(row) 
             
-            #FIRST HIDDEN LAYER TAKES WEIGHTS FROM INPUT LAYER
+            #FIRST HIDDEN LAYER TAKES WEIGHTS FROM INPUT LAYER #
             for neur in zip(np.nditer(self.hidden_layers[0].weights), np.nditer(self.hidden_layers[0].biases)):
-                    res = relu(self.neur_feed_forward(self.input_layer.weights.shape[0], neur[0], neur[1]))
-                    #neur[0]
-                    print(res)
-            '''
-            for layer in self.hidden_layers:
+                    self.hidden_layers[0].weights = self.neur_feed_forward(self.input_layer.weights.shape[0], neur[0].T, neur[1])
 
-                for neur in zip(layer.weights.shape[0], layer.biases.shape[0]):
-                    res = relu(self.neur_feed_forward())
+            '''
+            for pos, layer in enumerate(self.hidden_layers[1:]):
+
+                for neur in zip(np.nditer(layer.weights), np.nditer(layer.biases)):
+                    neur[0] = self.neur_feed_forward(self.input_layer.weights.shape[0], neur[0], neur[1])
             '''
         return
 
@@ -193,14 +149,15 @@ class NeuralNetwork:
         return res
 
     ''' Print the nodes '''
+    
     def __str__(self):
         res = f"INPUT LAYER: \n {str(self.input_layer)}\n "
 
         for pos, layer in enumerate(self.hidden_layers):
             res += f"LAYER {pos} \n" + str(layer) + "\n"
         res += "\n"
-        res += f"OUTPUT LAYER: \n {str(self.output_layer)} "
+       # res += f"OUTPUT LAYER: \n {str(self.output_layer)} "
 
         return res
-
+    
     
